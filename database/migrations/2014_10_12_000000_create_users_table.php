914dc28ea24cc1ab2c3157fa6faff5c306a46c03
anassebn02 <anass.ebnerradiy@gmail.com>
2023-10-20 22:31:44 +0100

    initial project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullName');
+            $table->string('email')->unique();
+            $table->string('phoneNumber')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
